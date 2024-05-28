5b61669181d98d42ed75f57ffdc0c6a98fdd722e
Jaysanghani08 <sanghanijay36@gmail.com>
2024-05-28 13:02:08 +0530

    face - detection (only) done

--- FrontEnd/Security/src/App.jsx
+++ FrontEnd/Security/src/App.jsx
@@ -3,6 +3,7 @@ import LoginForm from './Views/Login'
 import QrCode from './Views/QrCode'
 import { Route, Routes } from 'react-router-dom';
 import QrReader from './Views/ReadQR'
+import WebcamDemo from './Views/FaceDetection';
 
 
 // react router 
@@ -13,8 +14,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<h1>Home</h1>} />
                 <Route path="/login" element={<LoginForm />} />
-                <Route path='/qrcode' element={<QrCode />} />
+                <Route path='/qr-code' element={<QrCode />} />
                 <Route path='/qr-reader' element={<QrReader />} />
+                <Route path='/face-detection' element={<WebcamDemo />} />
+                <Route path="*" element={<h1>Not Found</h1>} />
             </Routes>
         </div>
     )
--- FrontEnd/Security/vite.config.js
+++ FrontEnd/Security/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    commonjs()
+  ],
+    resolve: {
+        alias: {
+        'react-use-face-detection': 'react-use-face-detection/build/index.js',
+        },
+    },
+});
