9909bcb1b5b6debd23f32193d5d5ee9973d95ec0
shubhampatel464 <94957047+shubhampatel464@users.noreply.github.com>
2024-06-01 14:01:22 +0000

    staff entry exit function done path setting remaining

---
+++ backEND/models/transactional/staff.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const staffSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    mobile: {
+        type: Number,
+        unique: true
+    },
+    uuid: {
+        type: String,
+        required: true
+    },
+    photo_exit:{
+        type: String
+    },
+    photo_entry:{
+        type: String
+    },
+    entry_time:{
+        type: String
+    },
+    exit_time:{
+        type: String
+    }
+});
+
+const staff = mongoose.model('staff_transactional', staffSchema);
+
+module.exports = staff;
