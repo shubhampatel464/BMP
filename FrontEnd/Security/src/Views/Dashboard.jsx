2b46babc84cba87b1fef32c5e9fdab7d09e74c5a
Jaysanghani08 <sanghanijay36@gmail.com>
2024-06-01 21:12:41 +0530

    yyg

--- FrontEnd/Security/src/Views/Dashboard.jsx
+++ FrontEnd/Security/src/Views/Dashboard.jsx
@@ -11,26 +11,28 @@ const Dashboard = () => {
             {/* <Example /> */}
 
             {/* heading : "Security on left side at top" */}
-            <div className='flex flex-col justify-between items-center h-screen w-screen py-6 '>
+            <div className='flex flex-col justify-evenly items-center h-screen w-screen py-6 '>
                 <h1 className='text-5xl font-bold text-blue5 '>Security</h1>
 
-                {/* 4 tiles, 2 in each row */}
-                <div className='flex justify-evenly items-center w-full h-full'>
-                    <div className='bg-white p-10 rounded-2xl shadow-2xl w-[200px] h-[200px] space-y-5'>
-                        <h1 className='text-2xl font-bold'>Face Detection</h1>
-                        <p className='text-gray-500'>Detect face using webcam.</p>
-                    </div>
-                    <div className='bg-white p-10 rounded-2xl shadow-2xl w-[200px] h-[200px] space-y-5'>
-                        <h1 className='text-2xl font-bold'>QR Code</h1>
-                        <p className='text-gray-500'>Generate QR code.</p>
-                    </div>
-                    <div className='bg-white p-10 rounded-2xl shadow-2xl w-[200px] h-[200px] space-y-5'>
-                        <h1 className='text-2xl font-bold'>Read QR Code</h1>
-                        <p className='text-gray-500'>Read QR code using webcam.</p>
-                    </div>
-                    <div className='bg-white p-10 rounded-2xl shadow-2xl w-[200px] h-[200px] space-y-5'>
-                        <h1 className='text-2xl font-bold'>Login</h1>
-                        <p className='text-gray-500'>Login to your account.</p>
+                {/* total 4 tiles, 2 tiles in 1st row and 2 tiles in 2nd row */}
+                <div className='flex flex-col justify-between items-center h-full w-full'>
+                    <div className='flex flex-wrap mt-12 justify-between items-center h-1/2'>
+                        <div className='flex flex-col gap-10 justify-between items-center w-1/2 h-full'>
+                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg shadow-xl p-20'>
+                                <h1 className='text-3xl font-bold'>Total</h1>
+                            </div>
+                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg mt-4 shadow-xl p-20'>
+                                <h1 className='text-3xl font-bold'>Total</h1>
+                            </div>
+                        </div>
+                        <div className='flex flex-col justify-between items-center w-1/2 h-full'>
+                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg shadow-xl p-20'>
+                                <h1 className='text-3xl font-bold'>Total</h1>
+                            </div>
+                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg mt-4 shadow-xl p-20'>
+                                <h1 className='text-3xl font-bold'>Total</h1>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
