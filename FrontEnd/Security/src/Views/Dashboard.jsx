59ca46bf714d5989b7df42651db82a9e2593fc73
Jaysanghani08 <sanghanijay36@gmail.com>
2024-06-01 21:43:29 +0530

    Security Dashobard done

--- FrontEnd/Security/src/Views/Dashboard.jsx
+++ FrontEnd/Security/src/Views/Dashboard.jsx
@@ -2,39 +2,41 @@ import React from 'react'
 import { Navbar } from '../Components/Navbar'
 import { StickyFooterMobile } from '../Components/StickyFooterMobile'
 import Example from '../Components/FormModal'
-
+import {useNavigate} from 'react-router-dom'
 
 const Dashboard = () => {
+
+    const navigate = useNavigate();
+
     return (
         <>
             <Navbar />
-            {/* <Example /> */}
-
-            {/* heading : "Security on left side at top" */}
-            <div className='flex flex-col justify-evenly items-center h-screen w-screen py-6 '>
+            <div className='flex flex-col justify-evenly items-center w-screen py-6 '>
                 <h1 className='text-5xl font-bold text-blue5 '>Security</h1>
 
                 {/* total 4 tiles, 2 tiles in 1st row and 2 tiles in 2nd row */}
-                <div className='flex flex-col justify-between items-center h-full w-full'>
-                    <div className='flex flex-wrap mt-12 justify-between items-center h-1/2'>
-                        <div className='flex flex-col gap-10 justify-between items-center w-1/2 h-full'>
-                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg shadow-xl p-20'>
-                                <h1 className='text-3xl font-bold'>Total</h1>
-                            </div>
-                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg mt-4 shadow-xl p-20'>
-                                <h1 className='text-3xl font-bold'>Total</h1>
-                            </div>
-                        </div>
-                        <div className='flex flex-col justify-between items-center w-1/2 h-full'>
-                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg shadow-xl p-20'>
-                                <h1 className='text-3xl font-bold'>Total</h1>
-                            </div>
-                            <div className='flex flex-col justify-center items-center w-[200px] h-1/2 rounded-lg mt-4 shadow-xl p-20'>
-                                <h1 className='text-3xl font-bold'>Total</h1>
-                            </div>
-                        </div>
-                    </div>
+                <div className='flex flex-col justify-center items-center h-full w-screen'>
+                    <div className='flex flex-wrap justify-center items-center h-1/2 w-screen '>
 
+                        <div className='flex flex-col gap-10 justify-between items-center h-full m-10'>
+                            <div className='flex flex-col justify-center items-center w-[290px] h-1/2 rounded-lg shadow-xl p-20 cursor-pointer	' onClick={() => navigate('/qr-reader')}>
+                                <h1 className='text-xl font-bold'>Scan ID</h1>
+                            </div>
+                            <div className='flex flex-col justify-center items-center w-[290px] h-1/2 rounded-lg shadow-xl p-20 cursor-pointer	'>
+                                <h1 className='text-xl font-bold'>Parent Visit</h1>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-col gap-10 justify-between items-center h-full m-10'>
+                            <div className='flex flex-col justify-center items-center w-[290px] h-1/2 rounded-lg shadow-xl p-20 cursor-pointer	'>
+                                <h1 className='text-xl font-bold'>General Visitors</h1>
+                            </div>
+                            <div className='flex flex-col justify-center items-center w-[290px] h-1/2 rounded-lg shadow-xl p-20 cursor-pointer	'>
+                                <h1 className='text-xl font-bold'>Records</h1>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
