acc2455f3d5e3df13201fb059593a7d5c67e107d
shubhampatel464 <94957047+shubhampatel464@users.noreply.github.com>
2024-06-01 15:43:11 +0000

    f

--- backEND/triggers/attendence/monthlyUpdate.js
+++ backEND/triggers/attendence/monthlyUpdate.js
@@ -6,15 +6,22 @@ const staff = require('../../models/static/staff/staff');
 
 const updateAttendence = async () => {
     try {
+
+        const data = await staff.find({});
+
+        const temp = data.forEach(async (element) => {
+            const 
+        });
+
         
-        
+
     } catch (error) {
         console.log("This is error from ./triggers/attendence/monthlyUpdate.js");
         console.log(error);
     }
 }
 
-
+module.exports = updateAttendence;
 
 
 
