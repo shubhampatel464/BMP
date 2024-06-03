0c486299c7b3abb6ca011e81a7f3723e0c6f27d9
shubhampatel464 <94957047+shubhampatel464@users.noreply.github.com>
2024-06-03 14:25:34 +0000

    logs api added done

---
+++ backEND/controllers/dataTables/getVisitorsLogs.js
@@ -0,0 +1,14 @@
+const visitors_logs = require('../../models/logs/visitor');
+
+const getVisitorsLogs = async (req, res) => {
+    try {
+        const logs = await visitors_logs.find().sort({ createdAt: -1 });
+        res.status(200).json(logs);
+    } catch (error) {
+        console.log('This is error from ./dataTables/getVisitorsLogs.js');
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+}
+
+module.exports = getVisitorsLogs;
--- backEND/routes/data/dataAPI.js
+++ backEND/routes/data/dataAPI.js
@@ -6,8 +6,20 @@ const router = express.Router();
 
 const getCurrentVisitors = require('../../controllers/dataTables/getCurrentVisitors');
 
+const getStudentLogs = require('../../controllers/dataTables/getStudentLogs');
+
+const getVisitorsLogs = require('../../controllers/dataTables/getVisitorsLogs');
+
+const getStaffLogs = require('../../controllers/dataTables/getStaffLogs');
+
 
 router.get('/getCurrentVisitors', getCurrentVisitors);
+
+router.get('/getStudentLogs', getStudentLogs);
+
+router.get('/getVisitorsLogs', getVisitorsLogs);
+
+router.get('/getStaffLogs', getStaffLogs);
 
 
 module.exports = router;
