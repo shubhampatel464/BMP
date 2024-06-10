99b7d9bc7653a3253f6aa80d0c52abcbb16ae8f6
shubhampatel464 <94957047+shubhampatel464@users.noreply.github.com>
2024-06-10 16:54:05 +0000

    warden login done and password update done

---
+++ backEND/middleware/hostelWardenAuth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const auth = async (req, res, next) => {
+    try {
+        // console.log(req.headers)
+        const token = req.headers.authorization;
+        const data = await jwt.verify(token, process.env.JWT_SECRET);
+        // console.log(data);
+        req.user = data;
+        next();
+    } catch (error) {
+        console.log("This is error from ./middleware/hostelWardenAuth.js");
+        console.log(error);
+        return res.status(401).json({
+            message: 'Auth failed'
+        });
+    }
+};
+
+
+module.exports = auth;
