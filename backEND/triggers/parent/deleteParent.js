c9e5752ae6f36f3b2f51ea1d1914bec47a160e2f
shubhampatel464 <94957047+shubhampatel464@users.noreply.github.com>
2024-06-11 13:20:01 +0000

    ww

---
+++ backEND/triggers/parent/deleteParent.js
@@ -0,0 +1,14 @@
+// const parent_transactional = require('../../models/transactional/parent');
+
+// const deleteParent = async (req, res) => {
+//     try {
+//         const 
+//         res.status(200).send();
+//     } catch (error) {
+//         console.log("This is error from ./controllers/parent/deleteParent.js");
+//         console.log(error);
+//         res.status(500).send(error);
+//     }
+// }
+
+// module.exports = deleteParent;
